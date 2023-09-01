aa3c62f049f71cd0de600620255b6e404bf9d3d6
muzakkir <mohd.saifi@nashtechglobal.com>
2023-09-01 15:59:34 +0530

    Adding a terratest file

---
+++ README.md
@@ -0,0 +1,17 @@
+# Terratest to check the existence of the Application gateway.
+
+## With the help of this template, you can run the terratest to check or test the application gateway existence on your azure account.
+
+## To run the code, you need to follow the below instructions:-
+
+### Step 1: You need to add your own values in these varibles.
+
+            	subscriptionID = "< >"
+                resourceGroupName = "< >"
+                appGatewayName = "< >"
+
+### Step 2: You need to run few commands to run the terratest:-
+
+        go mod init <>
+        go mod tidy
+        go test -v
---
+++ main_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"os/exec"
+	"testing"
+    "github.com/stretchr/testify/assert"
+
+)
+
+var (
+
+	subscriptionID = "< >"
+	resourceGroupName = "< >"
+	appGatewayName = "< >"
+	actualAppGateway = true
+	
+)
+
+
+
+func TestAppGateway(t *testing.T) {
+
+	accessToken, err := accessToken()
+	if err != nil {
+		fmt.Println("Error generating access token:", err)
+		return
+	}
+
+	expectedAppGateway, err := checkApplicationGatewayExists(subscriptionID, resourceGroupName, appGatewayName, accessToken)
+	
+	fmt.Print(expectedAppGateway)
+
+	t.Run("Existence", func(t *testing.T) {
+	assert.Equal(t, actualAppGateway, expectedAppGateway, "Not exisits")
+	})
+}
+
+
+
+
+func accessToken() (string, error) {
+	cmd := exec.Command("az", "account", "get-access-token", "--query", "accessToken", "--output", "tsv")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(output)), nil
+}
+
+
+
+
+func checkApplicationGatewayExists(subscriptionID, resourceGroupName, applicationGatewayName, accessToken string) (bool, error) {
+	url := fmt.Sprintf("https://management.azure.com/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Network/applicationGateways/%s?api-version=2019-09-01",
+		subscriptionID, resourceGroupName, applicationGatewayName)
+
+	req, err := http.NewRequest("GET", url, nil)
+	fmt.Print(req)
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		return true, nil
+	}
+
+	return false, nil
+}
+
